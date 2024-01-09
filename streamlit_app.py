20df7f4e167670c472eb402613e848dc240d4dc6
Moncef EL HILALI <125501945+moncefhilali@users.noreply.github.com>
2024-01-09 20:55:20 +0100

    Update streamlit_app.py
    Remove Sub Header

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title("My Parents new Healthy Dinner!")
 streamlit.header("Breakfast Menu")
-streamlit.subheader("Breakfast Menu sub ")
+
 streamlit.text("Omega 3 & Blueberry Oatmeal")
 streamlit.text("Kale, Spinach & Rocket Smoothie")
 streamlit.text("Hard-Boiled Free Range Egg")
